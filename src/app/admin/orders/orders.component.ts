426a233ebb27d05af87a86ea214b9901489777d8
Hussaini <qhussaini@users.noreply.github.com>
2021-03-30 10:04:13 +0530

    first-commit

---
+++ src/app/admin/orders/orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss']
+})
+export class OrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
